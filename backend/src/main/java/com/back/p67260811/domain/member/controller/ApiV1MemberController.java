aa4009a298ab8798b54ac0005136e7717e88bb97
songfox00 <songfox00@gmail.com>
2026-09-01 12:52:37 +0900

    ApiV1MemberController

---
+++ backend/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
@@ -0,0 +1,59 @@
+package com.back.p67260811.domain.member.controller;
+
+import com.back.p67260811.domain.member.dto.MemberDto;
+import com.back.p67260811.domain.member.entity.Member;
+import com.back.p67260811.domain.member.service.MemberService;
+import com.back.p67260811.global.dto.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class ApiV1MemberController {
+
+    private final MemberService memberService;
+
+    record JoinReqBody(
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String username,
+
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String password,
+
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String nickname
+    ) {
+    }
+
+    record JoinResBody(
+            MemberDto memberDto
+    ) {
+    }
+
+    @PostMapping()
+    public RsData<MemberDto> join(
+            @RequestBody @Valid JoinReqBody reqBody
+    ) {
+
+        Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
+
+        return new RsData(
+                "201-1",
+                "회원가입이 완료되었습니다. %s님 환영합니다.".formatted(reqBody.nickname),
+                new JoinResBody(
+                        new MemberDto(member)
+                )
+        );
+    }
+}
+
