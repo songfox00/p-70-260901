a49c1c17d36e3dd9da5d963cabcafa869c46278c
songfox00 <songfox00@gmail.com>
2026-09-02 12:30:07 +0900

    Authorization delete

--- backend/src/main/java/com/back/p67260811/domain/post/post/controller/ApiV1PostController.java
+++ backend/src/main/java/com/back/p67260811/domain/post/post/controller/ApiV1PostController.java
@@ -63,7 +63,7 @@ public class ApiV1PostController {
     @Transactional
     public RsData<PostDto> write(
             @Valid @RequestBody PostWriteReqBody reqBody,
-            @RequestHeader("Authorization") @NotBlank @Size(min=30, max=50) String apiKey
+            @RequestHeader("Authorization") @NotBlank String apiKey
     ) {
 
         String authorization = apiKey.substring(7);
@@ -96,7 +96,7 @@ public class ApiV1PostController {
     public RsData<Void> modify(
             @PathVariable int id,
             @Valid @RequestBody PostModifyReqBody reqBody,
-            @RequestHeader("Authorization") @NotBlank @Size(min=30, max=50) String apiKey
+            @RequestHeader("Authorization") @NotBlank String apiKey
     ) {
         String authorization = apiKey.substring(7);
         Member actor = memberService.findByApiKey(authorization).orElseThrow(
@@ -119,8 +119,16 @@ public class ApiV1PostController {
 
     @DeleteMapping("/{id}")
     public RsData<Void> delete(
-            @PathVariable int id
+            @PathVariable int id,
+            @RequestHeader("Authorization") @NotBlank String apiKey
     ) {
+        String authorization = apiKey.replace("Bearer ", "");
+
+        Member actor = memberService.findByApiKey(authorization).orElseThrow(() -> new ServiceException("401-1", "API 키가 올바르지 않습니다."));
+        Post post = postService.findById(id).get();
+
+        if(!actor.equals(post.getAuthor())) throw new ServiceException("403-1", "삭제 권한이 없습니다.");
+
         postService.delete(id);
 
         return new RsData<>(
--- backend/src/test/java/com/back/p67260811/domain/post/post/controller/ApiV1PostControllerTest.java
+++ backend/src/test/java/com/back/p67260811/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -1,5 +1,6 @@
 package com.back.p67260811.domain.post.post.controller;
 
+import com.back.p67260811.domain.member.repository.MemberRepository;
 import com.back.p67260811.domain.post.post.entity.Post;
 import com.back.p67260811.domain.post.post.repository.PostRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -30,6 +31,9 @@ public class ApiV1PostControllerTest {
 
     @Autowired
     private PostRepository postRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("글 다건 조회")
@@ -170,6 +174,7 @@ public class ApiV1PostControllerTest {
         ResultActions resultActions = mvc
                 .perform(
                         delete("/api/v1/posts/%d".formatted(targetId))
+                                .header("Authorization", "Bearer user1")
                 )
                 .andDo(print());
 
@@ -196,6 +201,7 @@ public class ApiV1PostControllerTest {
                 .perform(
                         post("/api/v1/posts")
                                 .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer user1")
                                 .content("""
                                         {
                                             "title": "%s",
@@ -227,6 +233,7 @@ public class ApiV1PostControllerTest {
                 .perform(
                         post("/api/v1/posts")
                                 .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer user1")
                                 .content("""
                                         {
                                             "title": "%s",
