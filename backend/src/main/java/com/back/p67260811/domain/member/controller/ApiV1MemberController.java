f7070a2c2a65a5699c101a50407907266c315c8e
songfox00 <songfox00@gmail.com>
2026-09-03 09:39:30 +0900

    my info

--- backend/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
+++ backend/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
@@ -5,14 +5,12 @@ import com.back.p67260811.domain.member.entity.Member;
 import com.back.p67260811.domain.member.service.MemberService;
 import com.back.p67260811.global.dto.RsData;
 import com.back.p67260811.global.exception.ServiceException;
+import com.back.p67260811.global.rq.Rq;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiV1MemberController {
 
     private final MemberService memberService;
+    private final Rq rq;
 
     record JoinReqBody(
             @NotBlank
@@ -94,6 +93,19 @@ public class ApiV1MemberController {
                     new MemberDto(actor),
                     actor.getApiKey()
                 )
+        );
+    }
+
+
+    @GetMapping("/me")
+    public RsData<MemberDto> me() {
+
+        Member actor = rq.getActor();
+
+        return new RsData(
+                "200-1",
+                "OK",
+                    new MemberDto(actor)
         );
     }
 }
